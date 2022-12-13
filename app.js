db9a4b5dcebc5535e27d70afcb7282fbcaecfa97
LokeshBatham <76842058+LokeshBatham@users.noreply.github.com>
2022-12-13 22:56:08 +0530

    update.

---
+++ app.js
@@ -0,0 +1,79 @@
+// jshint esversion: 6
+const express = require("express")
+const bodyparser = require("body-parser")
+const request = require("request")
+const https = require("https")
+
+const app = express()
+app.use(express.static("public"))
+app.use(bodyparser.urlencoded({ extended: true }))
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/", function (req, res) {
+
+    var firstname = req.body.fname;
+    var lastname = req.body.lname;
+    var Email = req.body.email;
+
+    console.log(firstname, lastname, Email);
+
+    var data = {
+        members: [
+            {
+                email_address: Email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstname,
+                    LNAME: lastname,
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data)
+
+    const url = "https://us21.api.mailchimp.com/3.0/lists/77bd240d1"
+
+    const options = {
+        method: "POST",
+        auth: "lokesh:26639ad4397b44483d5ebb670c5ce0bd-us21"
+    }
+
+    const request = https.request(url, options, function (response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html")
+        } else {
+            res.sendFile(__dirname + "/failure.html")
+        }
+
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+})
+
+// app.post("/failure", function(respon){
+//     // respon.redirect("/")
+//     // respon.sendFile(__dirname + "/signup.html")
+// })
+
+app.listen(process.env.PORT || 3000, function (req, res) {
+    console.log("Server is running on port 3000");
+})
+
+
+
+
+// API KEY
+// 26639ad4397b44483d5ebb670c5ce0bd-us21
+
+
+// List ID
+// 77bd240d1e
